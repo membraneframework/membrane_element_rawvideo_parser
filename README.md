e6af67dcf0d9bc46157f49b78db1e09558ad357c
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-10-19 14:04:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Membrane Multimedia Framework: Raw video parser Element
+
+[![Build Status](https://travis-ci.com/membraneframework/membrane-element-rawvideo-parser.svg?branch=master)](https://travis-ci.com/membraneframework/membrane-element-rawvideo-parser)
+
+This package provides [Membrane Multimedia Framework](https://membraneframework.org)
+element that can be used to parse data stream into raw video frames.
+Documentation is available at [HexDocs](https://hexdocs.pm/membrane_element_rawvideo_parser/)
+
+
+## Installation
+
+Add the following line to your `deps` in `mix.exs`. Run `mix deps.get`.
+
+```elixir
+{:membrane_element_rawvideo_parser, "~> 0.1"}
+```
+
---
+++ mix.exs
@@ -0,0 +1,64 @@
+defmodule Membrane.Element.RawVideo.Parser.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/membraneframework/membrane-element-rawvideo-parser"
+
+  def project do
+    [
+      app: :membrane_element_rawvideo_parser,
+      version: @version,
+      elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      description: "Membrane Multimedia Framework (Raw video parser)",
+      package: package(),
+      name: "Membrane Element: Raw video parser",
+      source_url: @github_url,
+      docs: docs(),
+      homepage_url: "https://membraneframework.org",
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      },
+      files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs", "c_src"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.19.0", only: :dev, runtime: false},
+      {:membrane_core, github: "membraneframework/membrane-core"},
+      {:membrane_caps_video_raw, github: "membraneframework/membrane-caps-video-raw"},
+      {:membrane_element_file,
+       github: "membraneframework/membrane-element-file",
+       branch: "feature/release_v0.2",
+       only: :test}
+    ]
+  end
+end
