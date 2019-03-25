ed59544e761fc588f1580c2dd32b1ce193a1f0c1
Mateusz Front <mateusz.front@swmansion.com>
2019-03-25 18:00:40 +0100

    removed obsolete import Supervisor.Spec

--- lib/membrane_element_rawvideo/parser_app.ex
+++ lib/membrane_element_rawvideo/parser_app.ex
@@ -3,8 +3,6 @@ defmodule Membrane.Element.RawVideo.Parser.App do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     children = []
 
     opts = [strategy: :one_for_one, name: __MODULE__]
