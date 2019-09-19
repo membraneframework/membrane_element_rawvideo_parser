4ba81212d7ef5d5583a875ffe7e9fdb36efbb156
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-09-19 16:00:28 +0200

    Update deps and bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.RawVideo.Parser.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @github_url "https://github.com/membraneframework/membrane-element-rawvideo-parser"
 
   def project do
@@ -51,9 +51,8 @@ defmodule Membrane.Element.RawVideo.Parser.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.19.0", only: :dev, runtime: false},
-      {:membrane_core,
-       github: "membraneframework/membrane-core", branch: "master", override: true},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:membrane_core, "~> 0.4.0"},
       {:membrane_caps_video_raw, "~> 0.1.0"},
       {:membrane_element_file, "~> 0.2.3", only: :test}
     ]
